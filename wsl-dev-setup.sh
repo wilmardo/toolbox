905ad855936555065d5e3cb17fbf592aaac26154
Wilmar den Ouden <wilmardo@users.noreply.github.com>
2019-07-16 13:44:05 +0200

    feat: Adds minikube to wsl setup

--- wsl-dev-setup.sh
+++ wsl-dev-setup.sh
@@ -40,3 +40,6 @@ sudo tar -zxC "/usr/bin/" --strip-components=1 docker/docker
 # Install docker-compose (https://github.com/docker/compose/releases/)
 curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# Instal minikube https://github.com/kubernetes/minikube/releases/tag/v1.2.0
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.2.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
