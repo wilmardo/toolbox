43d8bf0f172d0cd54f245915e8a51b7051be58e1
Wilmar den Ouden <wilmardo@users.noreply.github.com>
2019-08-29 20:57:57 +0200

    Update lin-setup.sh

--- lin-setup.sh
+++ lin-setup.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
-sudo apt-get install fonts-powerline chrome-gnome-shell
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get install curl fonts-powerline chrome-gnome-shell gnome-tweak-tool build-essential ultra-flat-icons
+
+
+/org/gnome/desktop/interface/gtk-theme
+  'Adwaita-dark'
+
+/org/gnome/desktop/interface/icon-theme
+  'Ultra-Flat'
+
+/org/gnome/shell/extensions/dash-to-dock/preferred-monitor
+  0
+
+/org/gnome/shell/extensions/dash-to-dock/multi-monitor
+  true
+
+/org/gnome/shell/extensions/dash-to-dock/dock-position
+  'BOTTOM'
+
+/org/gnome/shell/extensions/dash-to-dock/intellihide-mode
+  'FOCUS_APPLICATION_WINDOWS'
+
+/org/gnome/shell/extensions/dash-to-dock/intellihide
+  false
+
+/org/gnome/shell/extensions/dash-to-dock/require-pressure-to-show
+  false
+
+/org/gnome/shell/extensions/dash-to-dock/dash-max-icon-size
+  32
+
+/org/gnome/shell/extensions/dash-to-dock/apply-custom-theme
+  true
+
+/org/gnome/settings-daemon/plugins/xsettings/overrides
+  {'Gtk/ShellShowsAppMenu': <0>}
+
+/org/gnome/desktop/wm/preferences/button-layout
+  ':appmenu,minimize,maximize,close'
