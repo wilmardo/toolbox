1c1db09ccad6d00705d918306a183d9e09ff840e
Wilmar den Ouden <wilmardo@users.noreply.github.com>
2019-08-29 21:04:09 +0200

    Update lin-setup.sh

--- lin-setup.sh
+++ lin-setup.sh
@@ -3,6 +3,29 @@
 sudo add-apt-repository ppa:noobslab/icons
 sudo apt-get install curl fonts-powerline chrome-gnome-shell gnome-tweak-tool build-essential ultra-flat-icons
 
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
+
+echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
+
+sudo apt install codium
+
 
 /org/gnome/desktop/interface/gtk-theme
   'Adwaita-dark'
