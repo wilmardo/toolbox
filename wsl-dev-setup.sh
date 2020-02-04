7ad38faab5977602191c7c132b3fb6e07a21dbba
Wilmar den Ouden <wilmardo@users.noreply.github.com>
2020-02-04 09:53:34 +0100

    refactor: new wsl-dev-setup.sh

--- wsl-dev-setup.sh
+++ wsl-dev-setup.sh
@@ -8,28 +8,31 @@ fi
 
 # Setup mount to /c/ instead of /mnt/c/
 cat <<EOF > /etc/wsl.conf
-[automount]
-root = /
-options = "metadata"
+[network]
+[network]
+generateResolvConf = false
 EOF
 
-# Change homedir to windows user folder
-HOME_DIR=~
-USERNAME=$(whoami) # we assume here that windows and linux username match
-sed -i "s,$HOME_DIR,/c/Users/$USERNAME,g" /etc/passwd
+echo "nameserver 9.9.9.9" > /etc/resolv.conf
 
 # Update all
 sudo apt-get update
 sudo apt-get dist-upgrade
 
 # Install nessacry packages
-sudo apt-get install -y zsh git tmux python3-venv
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    zsh \
+    git \
+    tmux \
+    python3-venv
 
 # Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Setup zsh on launch
-chsh -s $(which zsh)
 
 # Setup nice dir colors in WSL
 mkdir -p term-config/dircolors-solarized
@@ -37,26 +40,33 @@ git clone https://github.com/seebi/dircolors-solarized term-config/dircolors-sol
 
 # Setup dotfiles
 git clone --bare git@github.com:wilmardo/dotfiles.git $HOME/.cfg
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME reset --hard
 
+# Setup docker
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Setup kubectl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Source new zsh config and delete old history
 source $HOME/.zshrc
-
-# Setup docker client only
-# https://docs.docker.com/install/linux/docker-ce/binaries/
-curl -sL https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz | \
-sudo tar -zvxC "/usr/local/bin" --strip-components=1 docker/docker
+rm -rf $HOME/.zsh_history
 
 # Install docker-compose (https://github.com/docker/compose/releases/)
-curl -sL https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+install_bin https://github.com/docker/compose/releases/download/1.25.4/docker-compose-Linux-x86_64 docker-compose
 
 # Instal kind https://github.com/kubernetes-sigs/kind/releases
-curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/v0.4.0/kind-linux-amd64 && chmod +x kind && sudo mv kind /usr/local/bin/
-
-#TODO: fix this
-# Install hugo https://github.com/gohugoio/hugo/releases
-curl -sL https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_extended_0.55.6_Linux-64bit.tar.gz | \
-sudo tar -zvxC "/usr/local/bin" hugo
+install_bin https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-linux-amd64 kind
 
 # Install helm
-curl -sL https://get.helm.sh/helm-v2.14.2-linux-amd64.tar.gz | \
-sudo tar -zvxC "/usr/local/bin" --strip-components=1 linux-amd64/helm
+install_bin https://get.helm.sh/helm-v3.0.3-linux-amd64.tar.gz helm
